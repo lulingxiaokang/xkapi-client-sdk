f5c1471374904310336e103f7f2ee0fab19d257c
llxk <1820472818@qq.com>
2023-08-24 23:47:26 +0800

    Initial commit

---
+++ src/main/java/com/llxk/xkapiclientsdk/XkapiClientSdkApplication.java
@@ -0,0 +1,13 @@
+package com.llxk.xkapiclientsdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XkapiClientSdkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XkapiClientSdkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/llxk/xkapiclientsdk/XkapiClientSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llxk.xkapiclientsdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XkapiClientSdkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
